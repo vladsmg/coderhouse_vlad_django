6e2e07a1fdc0050694ac367c2e12b6ce18a6d783
vladsmg <vladimirmellag@gmail.com>
2024-01-13 14:20:56 -0300

    clase terminada

--- project/config/urls.py
+++ project/config/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from .views import saludo,saludo2, nombre, fecha_hora , tirar_dados
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+   # path('saludo/', saludo),
+    #path('saludo2/', saludo2),
+    #path('nombre/<nombre>/<apellido>', nombre),
+    #path('fecha_hora/', fecha_hora),
+    #path('tirar_dados/', tirar_dados)
+    path("", include("core.urls"))
 ]
